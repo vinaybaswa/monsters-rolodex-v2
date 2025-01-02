86669080fbcf92d5cbb41b2ff77557a1a2bba76d
vinay baswa <vinaybaswa@gmail.com>
2025-01-01 23:29:50 -0500

    install typescript and start typing

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,10 +1,10 @@
-import './card.styles.css';
+import "./card.styles.css";
 
 const Card = ({ monster }) => {
   const { id, name, email } = monster;
 
   return (
-    <div className='card-container'>
+    <div className="card-container">
       <img
         alt={`monster ${name}`}
         src={`https://robohash.org/${id}?set=set2&size=180x180`}
